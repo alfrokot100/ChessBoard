db1ba6ec01dbba8355bea253d9a5c88be7ced829
Affe O <alfrokot100@student.kau.se>
2024-09-09 15:29:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en siffra är du snäll!");
+            String Number = Console.ReadLine();
+            int Result;
+
+            bool ConversionSucces = int.TryParse(Number, out Result);
+            if (ConversionSucces) { Console.WriteLine(Result); }
+            else { Console.WriteLine("Felaktig input! Måste vara en siffra"); }
+        }
+    }
+}
