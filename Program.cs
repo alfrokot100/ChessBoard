3c631a7ee89d0b686561e571b262d88a7fe66e28
Affe O <alfrokot100@student.kau.se>
2024-09-12 22:23:38 +0200

    New

--- Program.cs
+++ Program.cs
@@ -1,17 +1,37 @@
-﻿namespace ChessBoard
+﻿using System;
+
+namespace ChessBoard
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Mata in en siffra är du snäll!");
+            
+            //unicode to show the squares, and setting a unicode standard output
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
             String Number = Console.ReadLine();
-            int Result;
 
-            bool ConversionSucces = int.TryParse(Number, out Result);
-            if (ConversionSucces) { Console.WriteLine(Result); }
-            else { Console.WriteLine("Felaktig input! Måste vara en siffra");
-                Console.WriteLine("Test");
+            bool ConversionSucces = int.TryParse(Number, out int Result);
+            if (ConversionSucces)
+            {
+                for (int i = 0; i < Result; i++)
+                {
+                    for (int j = 0; j < Result; j++)
+                    {
+                        if ((i + j) % 2 == 0)
+                        {
+                            Console.Write("◼︎");
+                        }
+                        else
+                        {
+                            Console.Write("◻︎");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }          
+            else { Console.WriteLine("Felaktig input! Måste vara en siffra");                         
             }
         }
     }
